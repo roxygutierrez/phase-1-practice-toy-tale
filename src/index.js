cea8c5d691902f9332e8a794a3620eef342ef894
Roxy <114114468+roxygutierrez@users.noreply.github.com>
2022-12-01 17:33:59 -0600

    commit tree 7c733db56a0516ee7b1c274dc2e778c0784db07c

--- src/index.js
+++ src/index.js
@@ -12,4 +12,81 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  loadToys();
 });
+
+const clearToys = () => {
+  const parentNode = document.querySelector("#toy-collection");
+  while (parentNode.lastElementChild) {
+    parentNode.removeChild(parentNode.lastElementChild);
+  }
+};
+
+const renderToy = (toy) => {
+  const parentNode = document.querySelector("#toy-collection");
+  const cardDiv = document.createElement("div");
+  cardDiv.className = "card";
+  const h2Element = document.createElement("h2");
+  h2Element.textContent = toy.name;
+  cardDiv.append(h2Element);
+  const img = document.createElement("img");
+  img.src = toy.image;
+  img.className = "toy-avatar";
+  cardDiv.append(img);
+  const pElement = document.createElement("p");
+  pElement.textContent = `${toy.likes} likes`;
+  cardDiv.append(pElement);
+  const btn = document.createElement("button");
+  btn.addEventListener("click", likeFunc);
+  btn.className = "like-btn";
+  btn.id = toy.id;
+  btn.dataset.likes = toy.likes;
+  btn.textContent = "Like ❤️";
+  cardDiv.append(btn);
+  parentNode.append(cardDiv);
+};
+
+const loadToys = () => {
+  fetch("http://localhost:3000/toys")
+    .then((response) => response.json())
+    .then((toys) => {
+      toys.forEach(renderToy);
+    });
+};
+
+document.querySelector("form.add-toy-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newToy = {};
+  newToy.name = e.target.name.value;
+  newToy.image = e.target.image.value;
+  newToy.likes = 0;
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newToy),
+  })
+    .then((res) => res.json())
+    .then((toy) => renderToy(toy));
+});
+
+const likeFunc = (event) => {
+  const newNumberOfLikes = ++event.target.dataset.likes;
+  fetch(`http://localhost:3000/toys/${event.target.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ likes: newNumberOfLikes }),
+  }).then(() => {
+    event.target.parentNode.querySelector(
+      "p"
+    ).textContent = `${newNumberOfLikes} likes`;
+  });
+};
+
+const refreshToys = () => {
+  clearToys();
+  loadToys();
+};
